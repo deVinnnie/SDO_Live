a892f070d03609c0ac79b09f140637f89f946d36
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2015-08-03 18:34:52 +0200

    Request fullscreen on startup

--- js/main.js
+++ js/main.js
@@ -47,7 +47,25 @@ $(document).ready(
     init
 );
 
+
+// From: http://davidwalsh.name/fullscreen
+// Find the right method, call on correct element
+function launchFullScreen(element) {
+    if(element.requestFullScreen) {
+        element.requestFullScreen();
+    } else if(element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if(element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen();
+    }
+}
+
 function init(){
+    launchFullScreen(document.documentElement);
+    //Request Full Screen. Normally requires user interaction. Use Firefox and alter config so that:
+    //full-screen-api.allow-trusted-requests-only = false
+    //See: http://stackoverflow.com/questions/9454125/javascript-request-fullscreen-is-unreliable
+
     $("#metadata").hide();
     progressJs().start();
 

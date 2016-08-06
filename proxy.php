7d39a0c9c3c399a3abdda5287662a242344e657a
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-08-06 15:45:27 +0200

    Include character encoding in header

--- proxy.php
+++ proxy.php
@@ -1,11 +1,19 @@
 <?php
-	header("Content-Type:application/json");
+	header("Content-Type:application/json; charset=utf-8");
 	if(isset($_GET["channel"])){
 		$channel = $_GET["channel"];
 	}
 	else{
 		$channel = "aia_0094";
 	}
+
+	/*
+	Test code to simulate network error.
+	if($channel == "aia_0094"){
+		header("HTTP/1.0 404 Not Found");
+		exit();
+	}*/
+
 	$rss_feed = file_get_contents('http://sdo.gsfc.nasa.gov/feeds/' . $channel . '.rss');
 	$rss_feed = str_replace(array("\n", "\r", "\t"), "", $rss_feed);
 

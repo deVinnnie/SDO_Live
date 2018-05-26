d7fa413fb6022dc26fef10b80192e487f0031a6d
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2018-05-26 20:00:27 +0200

    Tweak font and image sizes

--- static/js/ProbeOverlay.js
+++ static/js/ProbeOverlay.js
@@ -2,6 +2,6 @@ export function ProbeOverlay(props){
   var src="images/spacecraft/spacecraft-" + props.current.sensor.toLowerCase() + "-overlay.png";
   
   return <div className="sensor">
-    <img id="spacecraft" src={src} width="250px"/>
+    <img id="spacecraft" src={src} width="350px"/>
   </div>;
 }

941611616d54d65f4fe72bced07aa438783e1f1f
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2020-01-31 20:14:02 +0100

    Limit curl operation to 30s

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 URL='https://sdo.gsfc.nasa.gov/assets/img/browse/'`date +%Y/%m/%d/`
 IMAGE_RESOLUTION='1024'
-curl "$URL" | grep '<a.*' | sed -n "s/^.*href=\"\(\S*\)\".*$/\1/p" | grep "_${IMAGE_RESOLUTION}_" | tac | head -n 50 > listing.txt
+curl --max-time 30 "$URL" | grep '<a.*' | sed -n "s/^.*href=\"\(\S*\)\".*$/\1/p" | grep "_${IMAGE_RESOLUTION}_" | tac | head -n 50 > listing.txt
